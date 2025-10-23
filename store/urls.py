6a7f4273d6052245f548b4596f149a01b6f42cdc
k-kimtaI <tezkim414@gmail.com>
2025-10-23 23:59:08 +0300

    Remove deleted files

--- store/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('cart/', views.cart_view, name='cart_view'),
-    path('add/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<str:key>/', views.remove_from_cart, name='remove_from_cart'),
-]
